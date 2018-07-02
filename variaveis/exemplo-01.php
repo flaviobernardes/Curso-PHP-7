2152081a0f88989eb8928fc7fc0c606b03d80e0a
flaviobernardes <flavio.bernardes@hotmail.com>
2018-07-02 00:09:38 -0300

    Iniciando estudo sobre variaveis

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.5">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Curso PHP 7">
+	<meta name="author" content="Flávio Gomes Bernardes">
+	<title>Curso de PHP 7 - variaveis</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/font-awesome.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300|Slabo+27px" rel="stylesheet">
+	<link rel="icon" href="../imagens/favicon/favicon.png">
+</head>
+<body>
+	<p>Uso de variáveis no PHP</p>
+	<?php
+	// Variavel do tipo string
+	$nome = "Flávio Sistema de informação";
+	echo 'O conteudo da variável $nome é..: ' . $nome;
+
+	// Usando var_dump para mostrar detalhes
+	echo '<br/><br/>Usando var_dump para mostrar detalhes da variavel<br/>';
+	var_dump($nome);
+
+	?>
+</body>
+</html>
